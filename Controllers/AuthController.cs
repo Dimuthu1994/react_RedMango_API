42db308db4571bae195d795181f3182dba48cb54
Dimuthu1994 <dimuthudiss6@gmail.com>
2024-02-01 16:09:15 +0530

    Auth controller dependencey injection

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RedMango_API.Data;
+using RedMango_API.Models;
 
 namespace RedMango_API.Controllers
 {
@@ -6,5 +9,19 @@ namespace RedMango_API.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly ApplicationDbContext _db;
+        private ApiResponse _response;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private string secretKey;
+        public AuthController(ApplicationDbContext db, IConfiguration configuration,
+            UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            secretKey = configuration.GetValue<string>("ApiSettings:Secret");
+            _response = new ApiResponse();
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
     }
 }
